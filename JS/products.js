42a2ee4371a7c6128a2a29cbae6bf03db18307f7
Mrq9rawi <moh.abdullah02@gmail.com>
2021-12-13 12:53:20 -0500

    alert fixed

--- JS/products.js
+++
@@ -1,8 +0,0 @@
-let productName = document.querySelectorAll(".products>div p");
-
-productName.forEach(function (ele) {
-	ele.addEventListener("click", function () {
-		window.localStorage.setItem("product", ele.textContent);
-		window.alert(`(${ele.textContent}) Has been added to your cart`);
-	});
-});
