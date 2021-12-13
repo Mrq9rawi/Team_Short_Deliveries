421d3cf2ee7385746d69c29945e3110a20ca8dfb
Mrq9rawi <moh.abdullah02@gmail.com>
2021-12-13 12:34:12 -0500

    Alert made for the home page

--- JS/products.js
+++ JS/products.js
@@ -3,5 +3,6 @@ let productName = document.querySelectorAll(".products>div p");
 productName.forEach(function (ele) {
 	ele.addEventListener("click", function () {
 		window.localStorage.setItem("product", ele.textContent);
+		window.alert(`(${ele.textContent}) Has been added to your cart`);
 	});
 });
