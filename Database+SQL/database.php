1e9827dd6ec70e3eb678fac13f6af64102b6e641
CShingiro <cshingiro7202@conestogac.on.ca>
2021-12-16 17:04:59 -0500

    Attempt at fix to database

--- Database+SQL/database.php
+++ Database+SQL/database.php
@@ -11,5 +11,5 @@
 
     $db_connect = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
         OR die('Could not connect to MySQL: '. mysqli_connect_error());
-    mysqli_set_charset($db, 'utf-8');
+    mysqli_set_charset($db_connect, 'utf-8');
 ?>
